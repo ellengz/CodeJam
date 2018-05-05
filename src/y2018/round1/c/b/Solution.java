50a0fea8322223d13d5a3748c47ffbcef0606c14
Ellen Gong <ellen3gz@gmail.com>
2018-05-05 21:49:18 +1000

    basic solution without statistics

--- src/y2018/round1/c/b/Solution.java
+++ src/y2018/round1/c/b/Solution.java
@@ -1,8 +1,7 @@
-package y2018.round1.c.c;
+package y2018.round1.c.b;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Solution {
@@ -13,8 +12,38 @@ public class Solution {
 
         int T = in.nextInt();
         for (int t = 1; t <= T; t++) {
+            int N = in.nextInt(); // N customers
+            if (N == -1) {
+                return;
+            } else {
+                int[] stock = new int[N];
+                for (int i = 0; i < N; i ++) {
+                    int D = in.nextInt(); // D prefer flavours
+                    if (D == 0) {
+                        System.out.println(-1);
+                    } else {
+                        int prefs[] = new int[D]; // each flavour ID in strictly increasing order
+                        for (int j = 0; j < D; j ++) {
+                            prefs[j] = in.nextInt();
+                        }
+                        boolean found = false;
+                        for (int j = 0; j < D; j ++) {
+                            if (stock[prefs[j]] == 0) { // find one in stock
+                                stock[prefs[j]] --; // update stock
+                                System.out.println(prefs[j]);
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            System.out.println(-1);
+                        }
+                    }
 
+                }
+            }
         }
 
     }
+
 }
